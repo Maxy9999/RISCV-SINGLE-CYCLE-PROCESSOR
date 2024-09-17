c50778a5917ec9552de592a731d38b1449b5b848
Kashyap63 <119111592+Kashyap63@users.noreply.github.com>
2024-09-17 14:11:13 +0530

    Update controller.v

--- modules/controller.v
+++ modules/controller.v
@@ -5,7 +5,7 @@ module controller (
     input [6:0]  op,
     input [2:0]  funct3,
     input        funct7b5,
-    input        Zero,
+    input        zero,res31,
     output       [1:0] ResultSrc,
     output       MemWrite,
     output       PCSrc, ALUSrc,
@@ -23,7 +23,15 @@ main_decoder    md (op, ResultSrc, MemWrite, Branch,
 alu_decoder     ad (op[5], funct3, funct7b5, ALUOp, ALUControl);
 
 // for jump and branch
-assign PCSrc = (Branch & Zero) | Jump;
+assign PCSrc = Jump | (Branch & (
+(funct3 == 3'b000 && zero) |//beq
+(funct3 == 3'b001 && ~zero) |//bnq
+(funct3 == 3'b100 && res31) |//blt
+(funct3 == 3'b101 && (~res31 | zero)) |//bge
+(funct3 == 3'b110 && res31) |//bltu //as they are unsigned anyway
+(funct3 == 3'b111 && (~res31 | zero))
+//bgeu
+));
 
 endmodule
 
