7fd536cd5864a18e7ac8b850079dc7f22eec9efd
Kashyap63 <119111592+Kashyap63@users.noreply.github.com>
2024-09-19 08:08:05 +0530

    added j-type instructions

--- modules/controller.v
+++ modules/controller.v
@@ -9,7 +9,7 @@ module controller (
     output       [1:0] ResultSrc,
     output       MemWrite,
     output       PCSrc, ALUSrc,
-    output       RegWrite, Jump,
+    output       RegWrite, Jump,Jalr,
     output [1:0] ImmSrc,
     output [3:0] ALUControl
 );
@@ -18,7 +18,7 @@ wire [1:0] ALUOp;
 wire       Branch;
 
 main_decoder    md (op, ResultSrc, MemWrite, Branch,
-                    ALUSrc, RegWrite, Jump, ImmSrc, ALUOp);
+                    ALUSrc, RegWrite, Jump,Jalr, ImmSrc, ALUOp);
 
 alu_decoder     ad (op[5], funct3, funct7b5, ALUOp, ALUControl);
 
@@ -28,7 +28,7 @@ assign PCSrc = Jump | (Branch & (
 (funct3 == 3'b001 && ~zero) |//bnq
 (funct3 == 3'b100 && res31) |//blt
 (funct3 == 3'b101 && (~res31 | zero)) |//bge
-(funct3 == 3'b110 && res31) |//bltu //as they are unsigned anyway
+(funct3 == 3'b110 && res31) |//bltu 
 (funct3 == 3'b111 && (~res31 | zero))
 //bgeu
 ));
